1ba8a5b9cfbf08cf7a207fee3d2a2780236f91c2
Herman Haggerty <dubh3124@gmail.com>
2026-06-06 23:53:27 -0500

    Add placeholder for PoolGame to allow test to compile but still fail

---
+++ src/8-ball-pool/game.ts
@@ -0,0 +1,9 @@
+export class PoolGame {
+    constructor(containerId: string) {
+        throw new Error('Not implemented');
+    }
+    get instance(): any {
+        return null;
+    }
+    destroy() {}
+}
